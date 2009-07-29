2e0db82c05b9a0770dd288dbcdbb3e75e57897df
Carl Lerche <carllerche@mac.com>
2009-07-29 01:55:57 -0700

    Crazy awesome backtracking goodness

--- lib/bundler/resolver.rb
+++ lib/bundler/resolver.rb
@@ -22,6 +22,11 @@ module Bundler
       result = catch(:success) do
         resolver = new(index)
         resolver.resolve(requirements, {})
+        puts "ERRORS:"
+        puts "======="
+        resolver.errors.each do |k,v|
+          puts "  #{k}"
+        end
         nil
       end
       result && result.values
@@ -29,7 +34,21 @@ module Bundler
 
     def initialize(index)
       @errors = {}
+      @stack  = []
       @index  = index
+    end
+    
+    def debug(str = "", nl = true)
+      return unless ENV['DEBUG']
+      if nl
+        puts str
+      else
+        print str
+      end
+    end
+    
+    def wait
+      STDIN.getc if ENV['DEBUG']
     end
 
     def resolve(reqs, activated)
@@ -38,22 +57,50 @@ module Bundler
       reqs = reqs.sort_by do |req|
         activated[req.name] ? 0 : @index.search(req).size
       end
+      
+      wait
+      debug "\e[2J\e[f", true
+      debug "Requirements:"
+      reqs.each do |r|
+        debug "  * #{r}"
+      end
+      debug
+      debug "Activated:"
+      activated.each do |k,v|
+        debug "  * #{v.full_name}"
+      end
+      debug
+      debug "-----"
 
       activated = activated.dup
       current   = reqs.shift
+      
+      debug "Attempting: #{current}"
 
       if existing = activated[current.name]
         if current.version_requirements.satisfied_by?(existing.version)
+          debug "  Existing: #{existing.full_name} -- SUCCESS"
           @errors.delete(existing.name)
           resolve(reqs, activated)
         else
           @errors[existing.name] = { :gem => existing, :requirement => current }
           parent = current.required_by.last || existing.required_by.last
-          throw parent.name
+          debug "  Existing: #{existing.full_name} -- FAIL"
+          debug "  BACKTRACK: #{parent.name}"
+          throw parent.name, existing.required_by.last.name
         end
       else
+        fail = []
         @index.search(current).reverse_each do |spec|
-          resolve_requirement(spec, current, reqs.dup, activated.dup)
+          fail << resolve_requirement(spec, current, reqs.dup, activated.dup)
+        end
+        fail.compact!
+        fail.uniq!
+        # If this is a root level dependency, then backtrack to the highest
+        # point on the stack that caused a conflict
+        if current.required_by.last.nil? && fail.any?
+          spot = @stack.reverse.detect { |i| fail.include?(i) }
+          throw spot
         end
       end
     end
@@ -61,6 +108,9 @@ module Bundler
     def resolve_requirement(spec, requirement, reqs, activated)
       spec.required_by.replace requirement.required_by
       activated[spec.name] = spec
+      
+      debug "  Activating: #{spec.full_name}"
+      debug "  Requirements: #{spec.dependencies.select { |d| d.type!=:development}.map{|d|d.to_s}.join(", ")}"
 
       spec.dependencies.each do |dep|
         next if dep.type == :development
@@ -68,9 +118,13 @@ module Bundler
         reqs << dep
       end
 
-      catch(requirement.name) do
+      debug "  SAVEPOINT: #{requirement.name}"
+      @stack << requirement.name
+      retval = catch(requirement.name) do
         resolve(reqs, activated)
       end
+      @stack.pop
+      retval
     end
 
   end
